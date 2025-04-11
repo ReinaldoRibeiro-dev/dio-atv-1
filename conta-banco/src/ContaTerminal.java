e2b190fee8c97a16070603a57a3733b640bd90a6
ReinaldoRibeiro-dev <reinaldoribeiro.dev@gmail.com>
2025-04-10 22:38:35 -0300

    Código funcional para desafio Banco terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -7,6 +7,22 @@ public class ContaTerminal {
         double saldo;
 
         System.out.print("Ola, por favor, digite seu nome: ");
-        
+        Scanner nomeScanner = new Scanner(System.in);
+        nome = nomeScanner.nextLine();
+
+        System.out.print("Agora digite sua agência: ");
+        Scanner agenciaScanner = new Scanner(System.in);
+        agencia = agenciaScanner.nextLine();
+
+        System.out.print("Agora digite o número da sua conta: ");
+        Scanner numeroScanner = new Scanner(System.in);
+        numero = numeroScanner.nextInt();
+
+        System.out.print("Agora digite seu Saldo em conta: ");
+        Scanner saldoScanner = new Scanner(System.in);
+        saldo = saldoScanner.nextDouble();
+
+        System.out.print("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
+
     }
 }
